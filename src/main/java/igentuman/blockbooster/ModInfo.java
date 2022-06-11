b0d4245884093133cfb314eb756c743418800ead
igentuman <igentuman@gmail.com>
2022-06-11 22:35:32 +0300

    Wrong dependencies fix

--- src/main/java/igentuman/blockbooster/ModInfo.java
+++ src/main/java/igentuman/blockbooster/ModInfo.java
@@ -6,11 +6,8 @@ public final class ModInfo {
   }
 
   public static final String MODID = "blockbooster";
-  public static final String NAME = "@NAME@";
+  public static final String NAME = "Blocks Booster";
   public static final String VERSION = "@VERSION@";
   public static final String MC_VERSION = "[1.12.2]";
-  public static final String DEPENDENCIES = "required-after:forge@[14.23.1,);"
-      + "required-after:jei;"
-      + "required-after:mtlib;"
-      + "required-after:immersiveengineering;";
+  public static final String DEPENDENCIES = "required-after:forge@[14.23.1,);";
 }
